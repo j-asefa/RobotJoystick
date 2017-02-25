bd91ab1df2e0d36bc8b1896ff0a0a0daad98e9ec
James Asefa <jamesasefa@gmail.com>
2017-02-25 15:59:54 -0800

    dumb

--- RobotJoystick/ViewController.swift
+++ RobotJoystick/ViewController.swift
@@ -15,23 +15,13 @@ class ViewController: UIViewController {
     var allowTX = true
     var lastPositionx: CGFloat = 255.0
     var lastPositiony: CGFloat = 255.0
-    var validTouch: Bool = true
+    //var validTouch: Bool = true
     
     //MARK: Properties
     @IBOutlet var outerView: UIView!
-    @IBOutlet weak var joystick: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 10, height: 10))
-        self.joystick = Joystick(frame: rect)*/
-        
-        let size:CGFloat = 70.0
-        joystick.bounds = CGRect(x: 0, y: 0, width: size / 2, height: size / 2)
-        joystick.layer.cornerRadius = size / 2
-        joystick.layer.borderWidth = 1
-        joystick.layer.borderColor = UIColor.red.cgColor
-        joystick.layer.backgroundColor = UIColor.red.cgColor
         
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.connectionChanged(_:)), name: NSNotification.Name(rawValue: BLEServiceChangedStatusNotification), object: nil)
        _ = btDiscoverySharedInstance
@@ -64,49 +54,22 @@ class ViewController: UIViewController {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
-            
-            joystick.bounds.width = joystick.bounds.width * 2
-            joystick.bounds.height = joystick.bounds.height * 2
-            
-            let position = touch.location(in: joystick)
-            
-            if (position.x < 0 || position.y < 0) {
-                validTouch = false
-                return // ignore touches outside view
-            } else if (position.x > joystick.bounds.size.width || position.y > joystick.bounds.size.height) {
-                validTouch = false
-                print("x: ")
-                print(position.x)
-                print("joystick width: ")
-                print(joystick.bounds.size.width)
-                
-                print("y: ")
-                print(position.y)
-                print("joystick height: ")
-                print(joystick.bounds.size.height)
-                return
-            } else {
-                validTouch = true
-                writePosition(position.x,position2: position.y)
-            }
+            let position = touch.location(in: outerView)
+            writePosition(position.x,position2: position.y)
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if (validTouch) {
-            if let touch = touches.first {
-                let position = touch.location(in: joystick)
-                writePosition(position.x,position2: position.y)
-            }
+        if let touch = touches.first {
+            let position = touch.location(in: outerView)
+            writePosition(position.x,position2: position.y)
         }
     }
     
     // want to send speed of 0 and reset joystick to middle
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.timerTXDelayElapsed()
-        if (validTouch) {
-            writePosition(CGFloat(0.0),position2: CGFloat(0.0))
-        }
+        writePosition(CGFloat(0.0),position2: CGFloat(0.0))
     }
     
     override func viewWillDisappear(_ animated: Bool) {
